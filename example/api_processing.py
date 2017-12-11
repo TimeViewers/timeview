9bc08f3526074775cd1a727f8d4bf6531dce6b8d
Alexander Kain <kaina@ohsu.edu>
2017-12-11 11:15:27 -0800

    remove stale reference

--- example/api_processing.py
+++ example/api_processing.py
@@ -13,27 +13,13 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 from timeview.api import Track, processing
 
 
-if 0:  # speech
-    wav_name = Path(__file__).with_name('speech-mwm.wav')
-    wav = Track.read(wav_name)
-    par_name = Path(__file__).with_name('speech-mwm.lab')
-    par = Track.read(par_name)
-
-    processor = processing.Filter()
-    processor.set_data({'wave': wav})
-    results, = processor.process()
-
-    print(results)
-
-
-# rodent
-par_name = Path(__file__).with_name('rodent-E1023.lab')
+wav_name = Path(__file__).with_name('speech-mwm.wav')
+wav = Track.read(wav_name)
+par_name = Path(__file__).with_name('speech-mwm.lab')
 par = Track.read(par_name)
-tmv_name = Path(__file__).with_name('rodent-E1023.tmv')
-tmv = Track.read(tmv_name)
 
-processor = processing.RodentCallClassifier()
-processor.set_data({'activity partition': par, 'peak track': tmv})
+processor = processing.Filter()
+processor.set_data({'wave': wav})
 results, = processor.process()
 
 print(results)
